1d793ab461715a787f21d1b71dc15924a1a9b11d
Wfuger <wfuger@gmail.com>
2016-10-06 18:25:19 -0500

    A tapping game in objective c

--- Tapper/ViewController.h
+++ Tapper/ViewController.h
@@ -10,6 +10,13 @@
 
 @interface ViewController : UIViewController
 
+@property (nonatomic) NSNumber *maxTaps;
+@property (nonatomic) NSNumber *currentTaps;
+@property (weak, nonatomic) IBOutlet UILabel *currentTapsLabel;
+@property (weak, nonatomic) IBOutlet UITextField *maxNumberOfTapsTextField;
+@property (weak, nonatomic) IBOutlet UIImageView *tapperLogoImg;
+@property (weak, nonatomic) IBOutlet UIButton *coinBtnOutlet;
+@property (weak, nonatomic) IBOutlet UIButton *playBtnOutlet;
 
 @end
 
--- Tapper/ViewController.m
+++ Tapper/ViewController.m
@@ -14,6 +14,7 @@
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -24,6 +25,61 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)coinBtnDidPress:(id)sender {
+    
+    _currentTaps = [NSNumber numberWithInt:[_currentTaps intValue] + 1];
+    [self updateTapsLabel];
+    
+    if ([self isGameOver]) {
+        [self restartGame];
+    }
+    
+}
 
+- (IBAction)playBtnDidPress:(id)sender {
+    
+    if ([_maxNumberOfTapsTextField text] && ![[_maxNumberOfTapsTextField text]isEqualToString:@""]) {
+        
+        NSLog(@"current taps label text: %@", [_maxNumberOfTapsTextField text]);
+        
+        // Hidding and showing what needs to change
+        [self hideNShow];
+        
+        // Setting our max taps from the text field
+        _maxTaps = [NSNumber numberWithInt:[_maxNumberOfTapsTextField text].intValue];
+        NSLog(@"MAX TAPS: %@",_maxTaps);
+        
+        // Set the current taps label to zero
+        _currentTapsLabel.text = @"0 Taps";
+        
+        
+    }
+}
+-(void) hideNShow {
+    [_tapperLogoImg setHidden:![_tapperLogoImg isHidden]];
+    [_currentTapsLabel setHidden:![_currentTapsLabel isHidden]];
+    [_maxNumberOfTapsTextField setHidden:![_maxNumberOfTapsTextField isHidden]];
+    [_playBtnOutlet setHidden:![_playBtnOutlet isHidden]];
+    [_coinBtnOutlet setHidden:![_coinBtnOutlet isHidden]];
+}
+-(void) restartGame {
+    _maxTaps = [NSNumber numberWithInt:0];
+    
+    _maxNumberOfTapsTextField.text = @"";
+    
+    [self hideNShow];
+}
+
+-(BOOL) isGameOver {
+    if (_currentTaps.intValue >= _maxTaps.intValue) {
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
+-(void) updateTapsLabel {
+    _currentTapsLabel.text = [NSString stringWithFormat:@"%@ Taps", _currentTaps];
+}
 
 @end
